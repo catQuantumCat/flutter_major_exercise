819f07a7540fa39c1a81bc8fb07b18abdde253fe
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-01-16 21:00:49 +0700

    Add: searchArticle bloc

--- lib/presentation/search_article/bloc/search_article_bloc.dart
+++ lib/presentation/search_article/bloc/search_article_bloc.dart
@@ -1,14 +1,27 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 import 'package:news_app/model/article.dart';
+import 'package:news_app/repository/news_repository.dart';
 
 part 'search_article_event.dart';
 part 'search_article_state.dart';
 
 class SearchArticleBloc extends Bloc<SearchArticleEvent, SearchArticleState> {
-  SearchArticleBloc() : super(SearchArticleInitial()) {
-    on<SearchArticleEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final newsRepo = NewsRepository();
+  SearchArticleBloc() : super(SearchArticleState()) {
+    on<TypeInQuery>(_typeInQuery);
+  }
+
+  FutureOr<void> _typeInQuery(event, emit) async {
+    try {
+      if (state.query.isEmpty) {
+        return;
+      }
+      emit(state.copyWith(isLoading: true));
+      final searchedArticle = await newsRepo.searchArticles(query: event.query);
+      emit(state.copyWith(isLoading: false, articles: searchedArticle));
+    } catch (error) {
+      emit(state.copyWith(error: error.toString()));
+    }
   }
 }
--- lib/presentation/search_article/bloc/search_article_event.dart
+++ lib/presentation/search_article/bloc/search_article_event.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'search_article_bloc.dart';
 
-@immutable
-sealed class SearchArticleEvent {}
+abstract class SearchArticleEvent {}
+
+class TypeInQuery extends SearchArticleEvent {
+  final String query;
+  TypeInQuery(
+    this.query,
+  );
+}
